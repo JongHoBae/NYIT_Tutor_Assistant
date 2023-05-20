fe8a65bcc1645282df3a9575571037b08c459647
Kyungmin Lee <innoleek@gmail.com>
2023-05-19 17:24:02 -0700

    ml

--- app/src/main/java/com/bers/nyittutorassistant/QuestionPage.java
+++ app/src/main/java/com/bers/nyittutorassistant/QuestionPage.java
@@ -2,32 +2,57 @@ package com.bers.nyittutorassistant;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.activity.ComponentActivity;
 
-public class QuestionPage extends ComponentActivity  {
+public class QuestionPage extends ComponentActivity implements AdapterView.OnItemSelectedListener {
+    EditText titleBox;
+    EditText hoursBox;
+    EditText descriptionBox;
+    Button finish;
+    Spinner dropdown;
+    TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {}
+        @Override
+        public void afterTextChanged(Editable s) {
+            updateFinishButtonState();
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.question_page);
 
+        finish = findViewById(R.id.finish);
+        titleBox = findViewById(R.id.titleBox);
+        hoursBox = findViewById(R.id.hoursBox);
+        descriptionBox = findViewById(R.id.descriptionBox);
 
-        Spinner dropdown = findViewById(R.id.tutorSpinner);
+        titleBox.addTextChangedListener(textWatcher);
+        hoursBox.addTextChangedListener(textWatcher);
+        descriptionBox.addTextChangedListener(textWatcher);
+
+        dropdown = findViewById(R.id.tutorSpinner);
 
         String[] items = new String[]{"Select Name", "Jongho", "Kyungmin"};
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
 
         dropdown.setAdapter(adapter);
-
-
-        Button finish = findViewById(R.id.finish);
+        dropdown.setOnItemSelectedListener(this);
 
         finish.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,5 +63,21 @@ public class QuestionPage extends ComponentActivity  {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        updateFinishButtonState();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {}
+
+    private void updateFinishButtonState() {
+        if (!titleBox.getText().toString().isEmpty() && !hoursBox.getText().toString().isEmpty() && !descriptionBox.getText().toString().isEmpty() && !dropdown.getSelectedItem().toString().equals("Select Name")) {
+            finish.setEnabled(true);
+        } else {
+            finish.setEnabled(false);
+        }
     }
 }
